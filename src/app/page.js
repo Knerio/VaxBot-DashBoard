5cf58e3185606ca03fbdfcd088829d0253adc29e
Dario <derioreal63@gmail.com>
2024-11-15 08:10:26 +0100

    fix page

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,18 @@
+"use client"
+
 import {IconBrandDiscord} from "@tabler/icons-react";
 import logoBanner from "./assets/logo-banner.jpg"
 import Image from "next/image";
+import {useEffect} from "react";
 
 export default function Home() {
+
+    useEffect(() => {
+        if (window.localStorage.getItem("token")) {
+            window.location.replace("/dashboard")
+        }
+    }, []);
+
     return (
         <div
             className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] ">
